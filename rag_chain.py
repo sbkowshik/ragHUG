eea61253908693dcb35ffa7b13ad395eaea03dde
kowshik <sbkowshikk@gmail.com>
2024-05-31 13:42:51 +0530

    Update rag_chain.py

--- rag_chain.py
+++ rag_chain.py
@@ -1,18 +1,19 @@
 import tempfile
 import shutil
+from operator import itemgetter
+from typing import List
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Qdrant
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnableParallel, RunnablePassthrough
 from langchain_community.embeddings import HuggingFaceInferenceAPIEmbeddings
 from langchain_community.llms import HuggingFaceEndpoint
-from langchain_community.document_loaders import PyPDFLoader
-from langchain_core.prompts import PromptTemplate
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
-from operator import itemgetter
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 
 TEMPLATE = """You're TextBook-Assistant. You're an expert in analyzing history and economics textbooks. You should only rely on {context} and on the previous message history as context, and from that you build a context and history-aware to reply. 
 MAKE SURE YOU MENTION THE NAME OF THE FILE ALONG WITH PAGE NUMBERS OF INFORMATION FROM THE METADATA AT THE END OF YOUR RESPONSE EVERYTIME IN THIS FORMAT [File Name : Page Number].
@@ -45,11 +46,7 @@ def determine_optimal_chunk_size(doc_length):
         chunk_size = 2000
         chunk_overlap = 500
     return chunk_size, chunk_overlap
-store={}
-def get_session_history(session_id: str) -> BaseChatMessageHistory:
-        if session_id not in store:
-            store[session_id] = ChatMessageHistory()
-        return store[session_id]
+
 def chunk_and_store_in_vector_store(docs, chunk_size, chunk_overlap, token, qurl, qapi):
     embeddings = HuggingFaceInferenceAPIEmbeddings(
         api_key=token, model_name="sentence-transformers/all-MiniLM-L6-v2"
@@ -65,10 +62,12 @@ def chunk_and_store_in_vector_store(docs, chunk_size, chunk_overlap, token, qurl
 
 def process_user_input(user_query, vectorstore, token, chat_history,sessionid):
     retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 6})
-    relevant_docs = retriever.get_relevant_documents(user_query)
 
-    context = format_docs(relevant_docs)
-
+    store = {}
+    def get_session_history(session_id: str) -> BaseChatMessageHistory:
+        if session_id not in store:
+            store[session_id] = ChatMessageHistory()
+        return store[session_id]
     llm = HuggingFaceEndpoint(
         huggingfacehub_api_token=token,
         repo_id="mistralai/Mixtral-8x7B-Instruct-v0.1",
@@ -79,15 +78,15 @@ def process_user_input(user_query, vectorstore, token, chat_history,sessionid):
         temperature=0.1,
         repetition_penalty=1
     )
+
     prompt_template = ChatPromptTemplate.from_messages(
     [("system", TEMPLATE),
     MessagesPlaceholder(variable_name="history"),
     ("human", "{input}")]
 )   
     context = itemgetter("question") | retriever | format_docs
-    
     first_step = RunnablePassthrough.assign(context=context)
-    chain = llm | prompt_template | first_step
+    chain = first_step | prompt_template | llm
     with_message_history = RunnableWithMessageHistory(
     chain,
     get_session_history=get_session_history,

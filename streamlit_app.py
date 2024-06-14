3334d992df6edfe9ff2affbd74f16858e03fb203
kowshik <sbkowshikk@gmail.com>
2024-06-14 22:36:22 +0530

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,14 @@
 import streamlit as st
 from rag_chain import load_doc_text, determine_optimal_chunk_size, chunk_and_store_in_vector_store, process_user_input
 import uuid
+import secrets
+import string
 
 token = st.secrets["HUGGINGFACEHUB_API_TOKEN"]
 qurl = st.secrets["QDRANT_URL"]
 qapi = st.secrets["QDRANT_API"]
 uapi = st.secrets["UNST_API"]
+alphabet = string.ascii_letters + string.digits
 
 def main():
     
@@ -20,7 +23,7 @@ def main():
         st.session_state['splits'] = None
 
     if 'session_id' not in st.session_state:
-        st.session_state['session_id'] = str(uuid.uuid4).replace('-','')
+        st.session_state['session_id'] = ''.join(secrets.choice(alphabet) for _ in range(32))
 
     if 'messages' not in st.session_state:
         st.session_state['messages'] = []
@@ -37,7 +40,7 @@ def main():
                         docs, doc_length = load_doc_text(source_doc,uapi)
                         chunk_size, chunk_overlap = determine_optimal_chunk_size(doc_length)
                         st.session_state['vectorstore'], st.session_state['splits'] = chunk_and_store_in_vector_store(
-                            docs, chunk_size, chunk_overlap, token=token, qapi=qapi, qurl=qurl
+                            docs, chunk_size, chunk_overlap, token=token, qapi=qapi, qurl=qurl,sid=st.session_state['session_id']
                         )
                     st.success("DOCs Processed")
                 else:
